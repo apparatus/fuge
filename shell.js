eb1e32e57a7d48920048fc620477fdc220955040
Matthew O'Connor <matthewocsecond@gmail.com>
2016-02-24 14:11:49 +0000

    added support for untail all

--- shell.js
+++ shell.js
@@ -348,11 +348,11 @@ module.exports = function() {
       if (com.command === 'start' || com.command === 'watch'||
       com.command === 'unwatch' || com.command === 'grep'||
       com.command === 'stop'  || com.command === 'info' || 
-      com.command === 'tail'){
+      com.command === 'tail' || com.command === 'untail'){
         inputStructure(com.command,'[process]',
           com.description, com.action, system);
       }
-      else if (com.command === 'debug' || com.command === 'untail') {
+      else if (com.command === 'debug') {
         inputStructure(com.command,'<process>',
           com.description, com.action, system);
       }
