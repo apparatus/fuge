b971654310e96d148643456908a77d9dbc0f44c3
gijoe50000 <gijoe50000@gmail.com>
2018-08-27 19:27:07 +0100

    Added groups: opt and crit. So that groups of processes can be started and stopped without affecting processes

--- commands.js
+++ commands.js
@@ -18,7 +18,6 @@ var _ = require('lodash')
 var CliTable = require('cli-table')
 require('colors')
 
-
 module.exports = function () {
   var _runner = null
   var _dns = null
@@ -67,7 +66,7 @@ module.exports = function () {
 
 
   var showInfo = function (args, system, cb) {
-    if (args.length > 0) {
+      if (args.length > 0) {
       if (args.length === 1) {
         _runner.preview(system, args[0], 'short', cb)
       } else if (args.length === 2 && args[1] === 'full') {
@@ -81,29 +80,26 @@ module.exports = function () {
   }
 
 
-  var stopProcess = function (args, system, cb) {
-    if (args.length === 1) {
-      if (args[0] === 'all') {
-        _runner.stopAll(system, cb)
-      } else {
-        _runner.stop(system, args[0], cb)
-      }
-    } else {
-      cb('usage: stop <process> | all')
-    }
-  }
-
 
   var startProcess = function (args, system, cb) {
     if (args.length === 1) {
-      if (args[0] === 'all') {
-        _runner.startAll(system, cb)
-      } else {
-        _runner.start(system, args[0], cb)
-      }
+      if (args[0] === 'all') { _runner.startAll(system, cb) } else
+      if (args[0] === 'opt' || args[1] === 'opt')  { startGroup(args, system, cb)}   else
+      if (args[0] === 'crit'|| args[1] === 'crit') { startGroup(args, system, cb)}   else
+         { _runner.start(system, args[0], cb) }
     } else {
-      cb('usage: start <process> | all')
-    }
+      cb('usage: start <process> | all')}
+  }
+
+
+  var stopProcess = function (args, system, cb) {
+    if (args.length === 1) {
+      if (args[0] === 'all') { _runner.stopAll(system, cb) } else
+      if (args[0] === 'opt' || args[1] === 'opt')  { stopGroup(args, system, cb)}   else
+      if (args[0] === 'crit'|| args[1] === 'crit') { stopGroup(args, system, cb)}   else
+      { _runner.stop(system, args[0], cb)}
+    } else {
+      cb('usage: stop <process> | all')}
   }
 
 
@@ -284,11 +280,105 @@ module.exports = function () {
   }
 
 
+  var optional=[]
+  var critical=[]
+
+  var startGroup = function (args, system, cb) {
+      if (args[0] === 'opt') {
+        for(var i=0;i<optional.length;i++){
+          _runner.start(system, optional[i], cb)
+        }
+      } else
+      if(args[0] === 'crit'){
+        for( var i=0;i<critical.length;i++){
+          _runner.start(system, critical[i], cb)
+        }
+      }
+     else {
+      console.info('Incorrect group name. Groups are opt and crit. usage: startg <group> ')
+      }
+
+
+    }
+
+  var stopGroup = function (args, system, cb) {
+      if (args[0] === 'opt') {
+        console.log('stopping ' +args[0]+' group')
+        for(var i=0;i<optional.length;i++){
+             _runner.stop(system, optional[i], cb)
+        }
+      } else
+      if(args[0] === 'crit'){
+        console.log('stopping ' +args[0]+' group')
+        for(var i=0;i<critical.length;i++){
+              _runner.stop(system, critical[i], cb)
+          }
+        }
+       else {
+      console.info('Incorrect group name. Groups are opt and crit. usage: stop <group> ')
+      }
+  }
+
+
+  var addToGroup = function(args, system, cb) {
+        if (args[0]=='opt') {optional.push(args[1]);  console.log('Added '+ args[1]+' to optional group')} else
+        if (args[1]=='opt') {optional.push(args[0]);  console.log('Added '+ args[0]+' to optional group')} else
+        if (args[0]=='crit') {critical.push(args[1]); console.log('Added '+ args[1]+' to critical group')} else
+        if (args[1]=='crit') {critical.push(args[0]); console.log('Added '+ args[0]+' to critical group')} else
+        {
+          console.log('Incorrect group name. Groups are opt and crit. usage: add [group] [process]. The order doesn\'t matter')
+        }
+    }
+
+
+  var removeFromGroup = function(args, system, cb) {
+    console.log(" ")
+    if (args[0]==='opt') {rem(optional, args[1])} else
+    if (args[1]==='opt') {rem(optional, args[0])} else
+    if (args[0]==='crit') {rem(critical, args[1])} else
+    if (args[1]==='crit') {rem(critical, args[0])} else
+    {  console.log('Please specify a group. [opt] or [crit]')}
+  }
+
+
+  function rem(arr, arg){
+    var exist=false;
+    for(var i=0;i<=arr.length;i++){
+      if(arg===arr[i]){
+        var index=arr.indexOf(arg)
+        arr.splice(index,1)
+        console.log('Removed ' +arg)
+        exist=true;
+      }
+    }
+    if(!exist){console.log(arg+' does not exist in this group')}
+  }
+
+
+
+  function showGroups(){
+    console.log('')
+    console.log('Critical Group: ')
+    console.log('--------------')
+    console.log(''+critical.join("\n"))
+    console.log('----------')
+    console.log('')
+
+    console.log('Optional Group:')
+    console.log('--------------')
+    console.log(''+optional.join("\n"))
+    console.log('----------')
+    console.log('')
+ }
+
   var _commands = {
     ps: {action: psList, description: 'list managed processes and containers, usage: ps'},
     info: {action: showInfo, sub: [], description: 'show process and container environment information, usage: info <process> [full]'},
-    start: {action: startProcess, sub: [], description: 'start processes, usage: start<process> | all'},
-    stop: {action: stopProcess, sub: [], description: 'stop processes, usage: stop <process> | all'},
+    start: {action: startProcess, sub: [], description: 'start processes, usage: start <process> | all \n Or start a group [opt] or [crit]. usage: start <group>'},
+    stop: {action: stopProcess, sub: [], description: 'stop processes, usage: stop <process> | all \n Or stop a group [opt] or [crit]. usage: start <group>'},
+    add:{action: addToGroup, sub:[], description: 'add a process to a group. usage: add <process> <group> or <group> <process>. \nThe order doesn\'t matter'},
+    remove:{action: removeFromGroup, sub:[], description: 'remove a process from a group. usage: remove <process> <group> or <group> <process>. \nThe order doesn\'t matter'},
+    show: {action: showGroups, sub:[], description: 'Show groups. usage: show'},
     restart: {action: restartProcess, sub: [], description: 'restart a single process, usage: restart <process>'},
     debug: {action: debugProcess, sub: [], description: 'start a process in debug mode, usage: debug <process>'},
     watch: {action: watchProcess, sub: [], description: 'turn on watching for a process, usage: watch <process> | all'},
@@ -315,6 +405,7 @@ module.exports = function () {
         _commands[key].sub = sub
       }
     })
+
     return _commands
   }
 
