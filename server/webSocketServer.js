9bb7ca4a59d4994ce7c8451e07b849fdadbddfad
meluko <meluko@gmail.com>
2019-03-05 10:13:29 +0000

    Added broadcast to ws server + some refactor

--- server/webSocketServer.js
+++ server/webSocketServer.js
@@ -7,8 +7,6 @@ require('./utils/jsExtensions')
 const catchLog = require('./utils/consoleLogCatcher')()
 const { parseTable } = require('./utils/parser')
 
-const app = express()
-const server = http.createServer(app)
 
 const PROCESS_LOG_REGEX = /\[([a-z-]+)\s-\s(\d+)]:(.*)/
 const PROCESS_EXIT_STATUS_REGEX = /\[[a-zA-Z-]*\]\sexit\s-\sstatus:/
@@ -17,59 +15,43 @@ const COMMAND_ARGS_REGEX = /([a-zA-Z-]+)\s?(.*)/
 const tableResultCommands = ['ps', 'zone']
 const needPsCommands = ['stop', 'start', 'restart']
 
+const app = express()
+const server = http.createServer(app)
+
+const options = {
+    server,
+    clientTracking: true
+}
+const wss = new WebSocket.Server(options);
+app.use('/', express.static(__dirname + '/public'))
 
 const originalStdoutWrite = process.stdout.write
 const originalStderrWrite = process.stderr.write
 
-function init(system, commands) {
-    const wss = new WebSocket.Server({ server });
-
-    app.use('/', express.static(__dirname + '/public'))
-
-    wss.on('connection', function connection(ws) {
-
-        wrapCommands(commands, ws)
-
-        process.stdout.write = (stream) => {
-            const [ ,processName, pid, message ] = stream
-                .removeANSIColors()
-                .safeMatch(PROCESS_LOG_REGEX)
-
-            ws.send(JSON.stringify({ fugeId: 'root', processName, pid, message: JSON.tryParse(message) }));
-        }
-
-        process.stderr.write = (stream) => {
-            if (stream.match(PROCESS_EXIT_STATUS_REGEX)) {
-                commands.ps.action([], system, () => {})
-            }
-        }
-
-        ws.on('close', () => {
-            unwrapCommands(commands)
-            process.stdout.write = originalStdoutWrite
-            process.stderr.writr = originalStderrWrite
-        })
-
-        ws.on('message', function (message) {
-            let [, commandName, args] = message.match(COMMAND_ARGS_REGEX)
-            const command = commands[commandName] || commands.shell
-
-            args = args.trim().split(' ').filter(it => !!it)
-            command.action(args, system, () => {})
-
-            if (needPsCommands.includes(commandName)) {
-                setTimeout(() => commands.ps.action([], system, () => {}), 1000)
-            }
-        });
-
+wss.broadcast = function broadcast(msg) {
+    wss.clients.forEach(function each(client) {
+        client.send(msg);
     });
+};
 
-    server.listen(process.env.PORT || 3000, () => {
-        console.log(`Server started on port ${server.address().port}`);
-    });
+process.stdout.write = (stream) => {
+    const [ ,processName, pid, message ] = stream
+        .removeANSIColors()
+        .safeMatch(PROCESS_LOG_REGEX)
+
+    wss.broadcast(JSON.stringify({ fugeId: 'root', processName, pid, message: JSON.tryParse(message) }));
 }
 
-function wrapCommands(commands, ws) {
+function getPortFromSystem(system) {
+    const {global: {http_server}} = system
+    if (!http_server || !http_server.port) {
+        return
+    }
+
+    return http_server.port
+}
+
+function wrapCommands(commands, wss) {
     Object.entries(commands).forEach(([command, properties]) => {
         const action = properties.action
         properties.originalAction = action
@@ -80,7 +62,7 @@ function wrapCommands(commands, ws) {
                 result = parseTable(result)
             }
 
-            ws.send(JSON.stringify(result))
+            wss.broadcast(JSON.stringify(result))
         }
     })
 }
@@ -90,6 +72,50 @@ function unwrapCommands(commands) {
         properties.action = properties.originalAction
         delete properties.originalAction
     })
+}
+
+function init(system, commands) {
+
+    wrapCommands(commands, wss)
+
+    process.stderr.write = (stream) => {
+        if (stream.match(PROCESS_EXIT_STATUS_REGEX)) {
+            commands.ps.action([], system, () => {})
+        }
+    }
+
+    const handleOnConnectionClose = () => {
+        if (wss.clients.size) {
+            return
+        }
+
+        unwrapCommands(commands)
+        process.stdout.write = originalStdoutWrite
+        process.stderr.write = originalStderrWrite
+    }
+
+    const handleOnMessage = (message) => {
+        let [, commandName, args] = message.match(COMMAND_ARGS_REGEX)
+        const command = commands[commandName] || commands.shell
+
+        args = args.trim().split(' ').filter(it => !!it)
+        command.action(args, system, () => {})
+
+        if (needPsCommands.includes(commandName)) {
+            setTimeout(() => commands.ps.action([], system, () => {}), 1000)
+        }
+    }
+
+    const handleNewConnection = (ws) => {
+        ws.on('close', handleOnConnectionClose)
+        ws.on('message', handleOnMessage);
+    }
+
+    wss.on('connection', handleNewConnection);
+
+    server.listen(process.env.PORT || getPortFromSystem(system) || 3000, () => {
+        console.log(`Server started on port ${server.address().port}`);
+    });
 }
 
 module.exports = {
